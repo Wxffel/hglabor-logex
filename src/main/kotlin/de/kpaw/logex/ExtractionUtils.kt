5ec70d3329751a4939a2e6ca9d8ab7335dcd7ba5
Wxffel <66750759+Wxffel@users.noreply.github.com>
2021-12-10 20:58:52 +0100

    structure update

--- src/main/kotlin/de/kpaw/logex/ExtractionUtils.kt
+++ src/main/kotlin/de/kpaw/logex/ExtractionUtils.kt
@@ -12,24 +12,50 @@ import java.util.zip.ZipFile
 import kotlin.io.path.Path
 
 const val HGLABOR_START_DATE = "2020-01-01-1"
-
 val CURRENT_DATE = LocalDate.now().toString()
 val SYSTEM_OS_NAME: String = System.getProperty("os.name")
 val USER_HOME: String = System.getProperty("user.home")
-
 val USER_DESKTOP_PATH = Path("$USER_HOME/Desktop")
-
+val BLC_LOGS_PATH = Path("blclient/minecraft")
 val MC_LOGS_PATH = if (SYSTEM_OS_NAME == "Windows 10") {
     Path("""$USER_HOME\AppData\Roaming\.minecraft\logs""")
 } else Path("$USER_HOME/.minecraft/logs")
 
-val BLC_LOGS_PATH = Path("blclient/minecraft")
-
-// or ISO_8859_1
-var actualCharset: Charset = StandardCharsets.UTF_8
+var actualCharset: Charset = StandardCharsets.UTF_8 // or ISO_8859_1
 
 val hgLaborIPs = arrayListOf("178.32.80.96", "213.32.61.248")
 val hgLaborDomains = arrayListOf("hglabor.de", "hglabor.lol", "pvplabor.net")
+
+object LogExPatterns {
+    val logNameAsDate = Regex("""\d{4}-[01][0-9]-[0-3][0-9]""")
+    val time = Regex("""([012]\d:[0-5]\d:[0-5]\d)""")
+    val connecting = Regex(""": (?i)connecting to [a-zA-Z\d.-]*""")
+    val stopping = Regex(""": Stopping!""")
+    val chatMessage = Regex(""": \[CHAT] (.*)""")
+    val privateChatMessages = arrayListOf(
+        // minec4ft_name ? to you ?  Das ist eine private Nachricht
+        // ? minec4ft_name ? to you ?  Peter Fox
+        // ? minec4ft_name ? minec4ft_name ?  787 meine Telefonnummer
+        // MSG ? minec4ft_name ? minec4ft_name ?  bist du ein mädchen?????
+        // you to ? minec4ft_name ?  Lass das niemandem erzählen!111
+        Regex("""(MSG )?(\? )?((\.?\w{3,16})|(you to)) \? ((\.?\w{3,16})|(to you)) \?  (.*)"""),
+        // You -> minec4ft_name: Kannst du HG kommen? Ist grad voll leer :(
+        // minec4ft_name -> You: Ähm...ich spiele lieber FFA hahaa
+        Regex("""((You)|(\.?\w{3,16})) -> ((You)|(\.?\w{3,16})): (.*)"""),
+        // You whisper to minec4ft_name: als ob XD
+        // You whisper to Nachts | minec4ft_name: ok
+        // You whisper to ? minec4ft_name: ups
+        Regex("""You whisper to ((.+ \| )|(\? ))?(\.?\w{3,16}): (.*)"""),
+        // minec4ft_name whispers to you: of course
+        // ? minec4ft_name whispers to you: so viele patterns
+        // RANG | minec4ft_name whispers to you: yes
+        Regex("""((.+ [|] )|(\? ))?(\.?\w{3,16}) whispers to you: (.*)"""),
+        // §d?§r §6you to §f? §minec4ft_name §8?§7  okyyyy bye
+        Regex("""§d\?§r §6you to §f\? §7(\.?\w{3,16}) §8\?§7  (.*)"""),
+        // §d?§r §minec4ft_name §f? §6to you §8?§7  tricks
+        Regex("""§d\?§r §7(\.?\w{3,16}) §f\? §6to you §8\?§7  (.*)""")
+    )
+}
 
 fun String.isHGLaborIP() = hgLaborDomains.any { lowercase().contains(it) } || hgLaborIPs.contains(this)
 fun String.isHGLaborPrivateMessage() = LogExPatterns.privateChatMessages.any { it.matches(this) }
--- src/main/kotlin/de/kpaw/logex/Utils.kt
+++ src/main/kotlin/de/kpaw/logex/Utils.kt
@@ -7,37 +7,6 @@ import java.nio.file.Files
 import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file.attribute.FileTime
 
-object LogExPatterns {
-    val logNameAsDate = Regex("""\d{4}-[01][0-9]-[0-3][0-9]""")
-    val time = Regex("""([012]\d:[0-5]\d:[0-5]\d)""")
-    val connecting = Regex(""": (?i)connecting to [a-zA-Z\d.-]*""")
-    val stopping = Regex(""": Stopping!""")
-    val chatMessage = Regex(""": \[CHAT] (.*)""")
-    val privateChatMessages = arrayListOf(
-        // minec4ft_name ? to you ?  Das ist eine private Nachricht
-        // ? minec4ft_name ? to you ?  Peter Fox
-        // ? minec4ft_name ? minec4ft_name ?  787 meine Telefonnummer
-        // MSG ? minec4ft_name ? minec4ft_name ?  bist du ein mädchen?????
-        // you to ? minec4ft_name ?  Lass das niemandem erzählen!111
-        Regex("""(MSG )?(\? )?((\.?\w{3,16})|(you to)) \? ((\.?\w{3,16})|(to you)) \?  (.*)"""),
-        // You -> minec4ft_name: Kannst du HG kommen? Ist grad voll leer :(
-        // minec4ft_name -> You: Ähm...ich spiele lieber FFA hahaa
-        Regex("""((You)|(\.?\w{3,16})) -> ((You)|(\.?\w{3,16})): (.*)"""),
-        // You whisper to minec4ft_name: als ob XD
-        // You whisper to Nachts | minec4ft_name: ok
-        // You whisper to ? minec4ft_name: ups
-        Regex("""You whisper to ((.+ \| )|(\? ))?(\.?\w{3,16}): (.*)"""),
-        // minec4ft_name whispers to you: of course
-        // ? minec4ft_name whispers to you: so viele patterns
-        // RANG | minec4ft_name whispers to you: yes
-        Regex("""((.+ [|] )|(\? ))?(\.?\w{3,16}) whispers to you: (.*)"""),
-        // §d?§r §6you to §f? §minec4ft_name §8?§7  okyyyy bye
-        Regex("""§d\?§r §6you to §f\? §7(\.?\w{3,16}) §8\?§7  (.*)"""),
-        // §d?§r §minec4ft_name §f? §6to you §8?§7  tricks
-        Regex("""§d\?§r §7(\.?\w{3,16}) §f\? §6to you §8\?§7  (.*)""")
-    )
-}
-
 object TerminalMessages {
     fun noFilesFound(path: String) {
         terminal.println(brightRed("ERROR: There are no files in directory $path"))
